9a3df9371a0583b93f6b0a77c95d2a96675f168e
sebastian_kup <sebastian.kuppa@web.de>
2022-08-29 18:24:08 +0200

    added EntryListView and EntryDetailView, cleanup code

--- entries/models.py
+++ entries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+
 class Entry(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
